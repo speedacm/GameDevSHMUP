07a3016037f764109182f3d6ebdbca30da712af1
wDvy <69913344+wDvy@users.noreply.github.com>
2022-10-10 15:12:43 -0400

    Reorganized enemies in main

--- Scenes/Health.gd
+++ Scenes/Health.gd
@@ -5,7 +5,7 @@ export (int) var health = 100 setget set_health
 
 func set_health(new_health: int):
 	health = clamp(new_health, 0, 100)
-### Emit signal health change
+### Emit signal health change in the future
 
 func get_health():
 	return health
