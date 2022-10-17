4f450946095516a44c0dbe3b81b188e321c33068
Elijah Spicer <eli.sushi.spicer@gmail.com>
2022-10-17 16:32:36 -0400

    remove animation tree

--- Scripts/TestPlayer.gd
+++ Scripts/TestPlayer.gd
@@ -1,7 +1,5 @@
 extends KinematicBody2D
 
-onready var animation_tree = $AnimationTree
-onready var state_machine = animation_tree.get("parameters/playback")
 onready var animation_player = $AnimationPlayer
 export var bulletScene : PackedScene
 
