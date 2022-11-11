0f973a7aa6c4aacc6c0add8e70324465a32582e1
wDvy <69913344+wDvy@users.noreply.github.com>
2022-11-11 10:58:24 -0500

    Added 3 Round Burst Fireballs
    not tied to frame rate :sunglasses:

---
+++ Scripts/Fireball.gd
@@ -0,0 +1,56 @@
+extends KinematicBody2D
+
+export var smokeScene : PackedScene
+export var bulletImpact : PackedScene
+
+var speed = 400
+var direction = Vector2.ZERO
+
+var parent = ''
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if parent == "mobs":
+		set_collision_layer_bit(31, true)
+		set_collision_mask_bit(0, true)
+		set_collision_mask_bit(1, false)
+		set_collision_mask_bit(2, true)
+	else:
+		set_collision_layer_bit(31, true)
+		set_collision_mask_bit(0, true)
+		set_collision_mask_bit(1, true)
+
+
+func set_speed(s):
+	speed = s
+
+
+func _process(delta):
+
+## Bullet Checks what group it is in and acts accordingly
+
+	var collisionResult = move_and_collide(direction * speed * delta)
+	if collisionResult != null:
+		
+		#Get group at collision
+		if collisionResult.collider.is_in_group("mobs"):
+			collisionResult.collider.health.set_health(collisionResult.collider.health.get_health()-10)
+		
+		if collisionResult.collider.is_in_group("player"):
+			collisionResult.collider.health.set_health(collisionResult.collider.health.get_health()-10)
+		
+		
+### Bullet Effects (Smoke and Impact)
+
+		var smoke = smokeScene.instance() as Particles2D
+		get_parent().add_child(smoke)
+		smoke.global_position = collisionResult.position
+		smoke.rotation = collisionResult.normal.angle()
+		
+		var impact = bulletImpact.instance() as Node2D
+		get_parent().add_child(impact)
+		impact.global_position = collisionResult.position
+		impact.rotation = collisionResult.normal.angle()
+		queue_free()
+		
--- Scripts/Flamethrower.gd
+++ Scripts/Flamethrower.gd
@@ -6,21 +6,69 @@ export var bulletScene : PackedScene
 
 onready var gunmodel = $GunModel
 onready var muzzle = $GunModel/muzzle
+onready var timer = $Bursttimer
+onready var cooldowntimer = $"Attack Cooldown"
 var weapontype = "base"
+
+
+## Weapon Variables
+var rateofburst = 0.15
+var burstamount = 2 # burst amount is n-1 what burst it is 3 round burst is 2
+var shotsfired = 0
 var ammo = 0
+
+## Attack Cool down
+var rateoffire = 1
+var canfire = true
 
 
 signal weapon_fired(bullet, location, direction)
 
+func set_ammo(new_ammo: int):
+	ammo = clamp(new_ammo, 0 ,100)
 
 func shoot(): 
+	if(canfire == true):
+		
+		## Basic Shoot Function
 		var bullet = bulletScene.instance() as Node2D
 		bullet.set('parent', 'player')
-		get_parent().add_child(bullet)
+		get_parent().get_parent().add_child(bullet)
 		emit_signal("weapon_fired",bullet)
 		bullet.global_position = $GunModel/muzzle.global_position
 		bullet.direction = (get_global_mouse_position() - global_position).normalized()
 		bullet.rotation = bullet.direction.angle()
+		
+		timer.start(rateofburst)
+		cooldowntimer.start(rateoffire)
+		canfire = false
+		
+		
+#func altshoot():  # Where to add different bullet behavior later  - Would be cool to have the other fireballs in the burst move different directions
+#	var bullet = bulletScene.instance() as Node2D
+#	bullet.set('parent', 'player')
+#	get_parent().get_parent().add_child(bullet)
+#	bullet.global_position = $GunModel/muzzle.global_position
+#	bullet.direction = (get_global_mouse_position() - global_position).normalized()
+#	bullet.rotation = bullet.direction.angle()
 
-func set_ammo(new_ammo: int):
-	ammo = clamp(new_ammo, 0 ,100)
+
+
+func _on_attackcooldown_timeout():
+	canfire = true
+	shoot()
+	shotsfired += 1
+	timer.stop()
+	if( shotsfired < burstamount):
+		timer.start()
+		
+	else:
+		shotsfired = 0
+		canfire = false
+		return
+
+
+
+func _on_Attack_Cooldown_timeout():
+	canfire = true
+	
