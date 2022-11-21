fc31f45bfc8ea0474eb0168f6294930dbe2abf08
wDvy <69913344+wDvy@users.noreply.github.com>
2022-11-21 15:54:21 -0500

    Made player way more tanky and moved enemies away

--- Scripts/Health.gd
+++ Scripts/Health.gd
@@ -4,7 +4,7 @@ extends Node2D
 export (int) var health = 100 setget set_health
 
 func set_health(new_health: int):
-	health = clamp(new_health, 0, 100)
+	health = clamp(new_health, 0, 1000)
 ### Emit signal health change in the future
 
 func get_health():
