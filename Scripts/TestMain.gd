7eb888b3b231663908fc50e6c96309129a91a9d1
jay marx <102194316+sirgroucho@users.noreply.github.com>
2022-11-21 12:33:35 -0500

    first push
    27x27 and 18x18 should be working, I'm gonna poll to see what orientations of 54x54 we really need.

--- Scripts/TestMain.gd
+++ Scripts/TestMain.gd
@@ -5,45 +5,47 @@ var parent
 
 # Var to make gen easier
 var x = 0
+var R27n = "res://Scenes/Rooms/27x27R.tscn"
+
 
 # Declare member variables here. Examples:
 # var a = 2
-var room_unit = 240
+var room_unit = 430
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if parent_path:
 		parent = get_node(parent_path)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 0, Vector2(0,0), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 2, Vector2(0,-1* room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 1, Vector2(0,-2*room_unit), 0 )
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 3, Vector2(0,-3*room_unit), 270)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 3, Vector2(1*room_unit,-3*room_unit), 90)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 5, Vector2(1*room_unit,-4*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 2, Vector2(-1*room_unit,-2*room_unit), 90)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 4, Vector2(-2*room_unit,-2*room_unit), 270)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 5, Vector2(-2*room_unit,-3*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 2, Vector2(-2*room_unit,-1*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 2, Vector2(-2*room_unit,0*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 3, Vector2(-2*room_unit,1*room_unit), 180)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 1, Vector2(-1*room_unit,1*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 5, Vector2(-1*room_unit,0*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 3, Vector2(-1*room_unit,2*room_unit), 180)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 2, Vector2(0*room_unit,1*room_unit), 90)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 3, Vector2(0*room_unit,2*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 2, Vector2(-2*room_unit,-1*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 1, Vector2(1*room_unit,1*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 3, Vector2(1*room_unit,0*room_unit), 270)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 4, Vector2(2*room_unit,0*room_unit), 90)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 3, Vector2(2*room_unit,1*room_unit), 90)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 2, Vector2(2*room_unit,-1*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 3, Vector2(2*room_unit,-2*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 2, Vector2(1*room_unit,-2*room_unit), 90)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 3, Vector2(0*room_unit,3*room_unit),180)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 1, Vector2(1*room_unit,3*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 5, Vector2(2*room_unit,3*room_unit), 90)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 2, Vector2(1*room_unit,4*room_unit), 0)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 5, Vector2(1*room_unit,5*room_unit), 180)
-		LoadInstance("res://Scenes/Rooms/12x12R.tscn", 2, Vector2(1*room_unit,2*room_unit), 0)
+		LoadInstance(R27n, 0, Vector2(0,0), 0)
+		LoadInstance(R27n, 2, Vector2(0,-1* room_unit), 0)
+		LoadInstance(R27n, 1, Vector2(0,-2*room_unit), 0 )
+		LoadInstance(R27n, 3, Vector2(0,-3*room_unit), 270)
+		LoadInstance(R27n, 3, Vector2(1*room_unit,-3*room_unit), 90)
+		LoadInstance(R27n, 5, Vector2(1*room_unit,-4*room_unit), 0)
+		LoadInstance(R27n, 2, Vector2(-1*room_unit,-2*room_unit), 90)
+		LoadInstance(R27n, 4, Vector2(-2*room_unit,-2*room_unit), 270)
+		LoadInstance(R27n, 5, Vector2(-2*room_unit,-3*room_unit), 0)
+		LoadInstance(R27n, 2, Vector2(-2*room_unit,-1*room_unit), 0)
+		LoadInstance(R27n, 2, Vector2(-2*room_unit,0*room_unit), 0)
+		LoadInstance(R27n, 3, Vector2(-2*room_unit,1*room_unit), 180)
+		LoadInstance(R27n, 1, Vector2(-1*room_unit,1*room_unit), 0)
+		LoadInstance(R27n, 5, Vector2(-1*room_unit,0*room_unit), 0)
+		LoadInstance(R27n, 3, Vector2(-1*room_unit,2*room_unit), 180)
+		LoadInstance(R27n, 2, Vector2(0*room_unit,1*room_unit), 90)
+		LoadInstance(R27n, 3, Vector2(0*room_unit,2*room_unit), 0)
+		LoadInstance(R27n, 2, Vector2(-2*room_unit,-1*room_unit), 0)
+		LoadInstance(R27n, 1, Vector2(1*room_unit,1*room_unit), 0)
+		LoadInstance(R27n, 3, Vector2(1*room_unit,0*room_unit), 270)
+		LoadInstance(R27n, 4, Vector2(2*room_unit,0*room_unit), 90)
+		LoadInstance(R27n, 3, Vector2(2*room_unit,1*room_unit), 90)
+		LoadInstance(R27n, 2, Vector2(2*room_unit,-1*room_unit), 0)
+		LoadInstance(R27n, 3, Vector2(2*room_unit,-2*room_unit), 0)
+		LoadInstance(R27n, 2, Vector2(1*room_unit,-2*room_unit), 90)
+		LoadInstance(R27n, 3, Vector2(0*room_unit,3*room_unit),180)
+		LoadInstance(R27n, 1, Vector2(1*room_unit,3*room_unit), 0)
+		LoadInstance(R27n, 5, Vector2(2*room_unit,3*room_unit), 90)
+		LoadInstance(R27n, 2, Vector2(1*room_unit,4*room_unit), 0)
+		LoadInstance(R27n, 5, Vector2(1*room_unit,5*room_unit), 180)
+		LoadInstance(R27n, 2, Vector2(1*room_unit,2*room_unit), 0)
 
 func LoadInstance(path, roomID, newPos, newRot):
 	x=0 
