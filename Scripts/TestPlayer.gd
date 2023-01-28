5b72a20c5285aa3c1c04e66660233c3f6e8e818b
Elijah Spicer <eli.sushi.spicer@gmail.com>
2023-01-27 22:39:11 -0500

    centered the camera

--- Scripts/TestPlayer.gd
+++ Scripts/TestPlayer.gd
@@ -125,12 +125,14 @@ func _on_RoomDetector_area_entered(area: Area2D) -> void:
 	
 	print("Enter Room")
 	
-	var collision_shape = area.get_node("RoomCollider")
+	var collision_shape = area.get_node("CollisionShape2D")
 	var size = collision_shape.shape.extents * 2 * 2
+	var x_offset = 430 
+	var y_offset = 215
 	
 	var cam = $Camera2D
-	cam.limit_top = collision_shape.global_position.y - size.y/2
-	cam.limit_left = collision_shape.global_position.x - size.x/2
+	cam.limit_top = collision_shape.global_position.y - y_offset - size.y/2
+	cam.limit_left = collision_shape.global_position.x + x_offset - size.x/2
 	
 	cam.limit_bottom = cam.limit_top + size.y
 	cam.limit_right = cam.limit_left + size.x
