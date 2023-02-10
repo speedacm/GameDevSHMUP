8595ff450bd249f2c89802be801d0f44d86eee96
Devin Ward <69913344+wDvy@users.noreply.github.com>
2023-02-10 16:54:20 -0500

    Added Death Screen when player dies (#52)
    * Added Death Screen when player dies
    
    Lets player know they died and gives a button to reset scene.
    
    * Added Death Screen when player dies
    
    Lets player know they died and gives a button to reset scene.
    
    * Update to fix with aspect ratio

--- Scripts/Test_Player.gd
+++ Scripts/Test_Player.gd
@@ -20,6 +20,7 @@ var weaponbehavior
 signal pickuprequest
 signal hphudupdate(new_health) 
 signal ammohudupdate(ammo, equippedweapon)
+signal playerDied()
 
 func _ready():
 	#Sets Collision Layers
@@ -50,7 +51,7 @@ func _unhandled_input(event):
 	if (event.is_action_pressed("shoot")):
 		equippedweapon.shoot()
 
-### Check if Dead
+
 func _physics_process(_delta):
 	get_input()
 	velocity = move_and_slide(velocity)
@@ -70,8 +71,11 @@ func _physics_process(_delta):
 		animation_player.play("walk_up")
 	else:
 		animation_player.play("idle_right")
+		
 	if health.health <= 0:
-		queue_free()
+		emit_signal("playerDied")
+		self.visible = false
+
 
 
 ### 
