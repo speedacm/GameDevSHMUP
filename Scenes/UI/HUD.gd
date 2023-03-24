d7a5ab8d10b088946bbf5aaeaa9b67514f31d7ae
Devin Ward <69913344+wDvy@users.noreply.github.com>
2023-03-24 09:03:50 -0400

    update branch (#74)
    * Update GitHub actions (#12)
    
    * Bump workflow versions
    
    * Remove depricated input, update Godot version to latest
    
    * Update godot version for workflow
    
    * Update preset config
    
    * Fog of war (#73)
    
    * Refactored HUD
    
    * Added temp fog of war text
    
    * changed window
    
    ---------
    
    Co-authored-by: Collin Dewey <76862862+LegitMagic@users.noreply.github.com>

---
+++ Scenes/UI/HUD.gd
@@ -0,0 +1,32 @@
+extends CanvasLayer
+
+var max_value = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$healthBar.value = max_value
+	pass # Replace with function body.
+
+func _on_Player_hphudupdate(new_health):
+	$healthBar.value = new_health
+	pass # Replace with function body.
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _on_Player_ammohudupdate(ammo, equippedweapon):
+	if(equippedweapon.type == "Flamethrower"):
+		$weaponLabel.text = "Flames: "
+	elif(equippedweapon.type == "Peashooter"):
+		$weaponLabel.text = "Peas "
+	if(ammo != 0):
+		$ammoCounter.visible = true
+		$ammoCounter.text = ammo as String
+	elif(ammo == 0):
+		$ammoCounter.visible = false
+	pass # Replace with function body.
+	
+func _on_Player_playerDied():
+	$"Death Screen".visible = true;
+	pass # Replace with function body.
+
--- Scripts/Label.gd
+++ Scripts/Label.gd
@@ -1,6 +1,5 @@
 extends Label
 
-
 func _process(delta: float) -> void:
 	set_text("FPS " + String(Engine.get_frames_per_second()))
 
